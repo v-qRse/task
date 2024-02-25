8320e2f4b2b182c16712255471cedd3c8730e5b8
User v_qrse <michin20k@gmail.com>
2024-02-25 23:25:16 +0300

    Correction of code formatting, prime number search tasks

--- Base/src/main/java/org/example/Main.java
+++ Base/src/main/java/org/example/Main.java
@@ -2,53 +2,52 @@ package org.example;
 
 public class Main {
    public static void main(String[] args) {
-      Task_1();
-      Task_2();
-      Task_3();
-      Task_4();
+      PrintSequenceMultiplySevenMultiplier();
+      PrintMaximumAndMeanRandomValues();
+      PrintPrimeNumbers();
+      PrintRectangleFromStars();
    }
 
-   static void Task_1() {
-      for (int i = 1; i <= 14; i++){
-         System.out.print(7*i + " ");
+   private static void PrintSequenceMultiplySevenMultiplier() {
+      for (int multiplier = 1; multiplier <= 14; multiplier++){
+         System.out.print(7*multiplier + " ");
       }
       System.out.println();
    }
 
-   static void Task_2() {
-      int n = 30;
-      double[] array = new double[n];
-      double max = 0, sum = 0;
+   private static void PrintMaximumAndMeanRandomValues() {
+      double[] arrayRandomValues = new double[30];
+      double maxValuesArray = 0;
+      double sumValuesArray = 0;
 
-      for(double values: array) {
-         values = Math.random();
-         max = Math.max(max, values);
-         sum += values;
+      for(double value: arrayRandomValues) {
+         value = Math.random();
+         maxValuesArray = Math.max(maxValuesArray, value);
+         sumValuesArray += value;
       }
-      System.out.println(max);
-      System.out.println(sum/n);
+      System.out.println(maxValuesArray);
+      System.out.println(sumValuesArray/arrayRandomValues.length);
    }
 
-   static void Task_3() {
-      int max, n;
-      for (int i = 2; i <= 100; i++) {
-         max = (int) Math.round(Math.sqrt(i));
-         n = 2;
-         while (i%n != 0 && n <= max)
-            n++;
-         if (n > max)
-            System.out.println(i);
+   private static void PrintPrimeNumbers() {
+      for (int number = 2; number <= 100; number++) {
+         int divisor = (int) Math.round(Math.sqrt(number));
+         while (number%divisor != 0) {
+            divisor--;
+         }
+         if (divisor == 1) {
+            System.out.println(number);
+         }
       }
    }
 
-   static void Task_4() {
-      int n = 3;
-      int m = 5;
-      char ch = '*';
-
-      for (int i = 0; i < n; i++) {
-         for (int j = 1; j < m; j++) {
-            System.out.print(ch + " ");
+   final static int Length_Rectangle = 5;
+   final static int Wight_Rectangle = 3;
+   final static char Char_Star = '*';
+   private static void PrintRectangleFromStars() {
+      for (int wight = 0; wight < Wight_Rectangle; wight++) {
+         for (int length = 0; length < Length_Rectangle; length++) {
+            System.out.print(Char_Star + " ");
          }
          System.out.println();
       }
