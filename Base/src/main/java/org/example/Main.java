adb127cd9240dbef072d1aa27c9f1590b4e575dd
User v_qrse <michin20k@gmail.com>
2024-03-04 22:07:36 +0300

    Correction of code formatting

--- Base/src/main/java/org/example/Main.java
+++ Base/src/main/java/org/example/Main.java
@@ -1,21 +1,25 @@
 package org.example;
 
 public class Main {
+   private final static int Length_Rectangle = 5;
+   private final static int Wight_Rectangle = 3;
+   private final static char Char_Star = '*';
+
    public static void main(String[] args) {
-      PrintSequenceMultiplySevenMultiplier();
-      PrintMaximumAndMeanRandomValues();
-      PrintPrimeNumbers();
-      PrintRectangleFromStars();
+      printSequenceMultiplySevenMultiplier();
+      printMaximumAndMeanRandomValues();
+      printPrimeNumbers();
+      printRectangleFromStars();
    }
 
-   private static void PrintSequenceMultiplySevenMultiplier() {
+   private static void printSequenceMultiplySevenMultiplier() {
       for (int multiplier = 1; multiplier <= 14; multiplier++){
          System.out.print(7*multiplier + " ");
       }
       System.out.println();
    }
 
-   private static void PrintMaximumAndMeanRandomValues() {
+   private static void printMaximumAndMeanRandomValues() {
       double[] arrayRandomValues = new double[30];
       double maxValuesArray = 0;
       double sumValuesArray = 0;
@@ -29,7 +33,7 @@ public class Main {
       System.out.println(sumValuesArray/arrayRandomValues.length);
    }
 
-   private static void PrintPrimeNumbers() {
+   private static void printPrimeNumbers() {
       for (int number = 2; number <= 100; number++) {
          int divisor = (int) Math.round(Math.sqrt(number));
          while (number%divisor != 0) {
@@ -41,10 +45,7 @@ public class Main {
       }
    }
 
-   final static int Length_Rectangle = 5;
-   final static int Wight_Rectangle = 3;
-   final static char Char_Star = '*';
-   private static void PrintRectangleFromStars() {
+   private static void printRectangleFromStars() {
       for (int wight = 0; wight < Wight_Rectangle; wight++) {
          for (int length = 0; length < Length_Rectangle; length++) {
             System.out.print(Char_Star + " ");
